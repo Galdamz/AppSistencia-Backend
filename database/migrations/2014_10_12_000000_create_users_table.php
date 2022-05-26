79d28a1f27727cfbf847fa1351fa2f393bc23b9a
Galdámez <alberto.galdamez@salud.gob.sv>
2022-05-26 09:24:18 -0600

    Model: Users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('uid');
+            $table->foreign("role_id")->references("id")->on("roles");
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
